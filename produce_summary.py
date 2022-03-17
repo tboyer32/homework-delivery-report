c5c80d14155ab84743b25b7214b51eac09df897e
Tiffany Boyer <tboyercode@gmail.com>
2022-03-16 22:17:49 -0500

    fix and condense code

--- produce_summary.py
+++ produce_summary.py
@@ -1,40 +1,18 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+def process_deliveries(the_file, day):
+    print(f"Day {day}")
+    
+    the_file = open("um-deliveries-day-1.txt")
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+        print(f"Delivered {count} {melon}s for a total of ${amount}")
+    the_file.close()
 
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
+process_deliveries("um-deliveries-day-1.txt", 1)
+process_deliveries("um-deliveries-day-2.txt", 2)
+process_deliveries("um-deliveries-day-3.txt", 3)
